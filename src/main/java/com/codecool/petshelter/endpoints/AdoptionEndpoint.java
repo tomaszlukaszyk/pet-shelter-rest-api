57fc6a0659c5a9b5623e3c588823a8736c203266
Tomasz Łukaszyk <el.moue@gmail.com>
2018-11-22 21:44:56 +0100

    Add AdoptionEndpoint class

---
+++ src/main/java/com/codecool/petshelter/endpoints/AdoptionEndpoint.java
@@ -0,0 +1,63 @@
+package com.codecool.petshelter.endpoints;
+
+import com.codecool.petshelter.dao.AdoptionDao;
+import com.codecool.petshelter.model.Adoption;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("adoptions")
+public class AdoptionEndpoint {
+
+    private AdoptionDao dao;
+
+    @Inject
+    public AdoptionEndpoint(AdoptionDao dao) {
+        this.dao = dao;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllAdoptions() {
+        List<Adoption> adoptions = dao.getAllAdoptions();
+        return Response.ok(adoptions).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addAdoption(Adoption adoption) {
+        long id = dao.addAdoption(adoption);
+        if (id < 0) return Response.status(400).entity("Adoption already exists").build();
+        return Response.ok("Adoption added with id = " + id).build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateAdoption(Adoption adoption) {
+        if (dao.updateAdoption(adoption)) {
+            return Response.ok("Adoption updated").build();
+        }
+        return Response.status(404).entity("Adoption you are trying to change dose not exist in database").build();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAdoptionByID(@PathParam("id") long id) {
+        Adoption adoption = dao.getAdoptionById(id);
+        if(adoption == null) return Response.status(404).entity("There is no adoption in database with id = " + id).build();
+        return Response.ok(adoption).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response deleteAdoption(long id) {
+        if(dao.removeAdoption(id)) {
+            return Response.ok("Deleted successfully").build();
+        }
+        return Response.status(400).entity("Could not delete adoption").build();
+    }
+}

def6eaca00ad63782238a999ae24514ea66bea0c
Tomasz Łukaszyk <el.moue@gmail.com>
2018-11-22 22:59:46 +0100

    Add method to add pet to adoption

--- src/main/java/com/codecool/petshelter/endpoints/AdoptionEndpoint.java
+++ src/main/java/com/codecool/petshelter/endpoints/AdoptionEndpoint.java
@@ -1,7 +1,10 @@
 package com.codecool.petshelter.endpoints;
 
 import com.codecool.petshelter.dao.AdoptionDao;
+import com.codecool.petshelter.dao.PetDao;
 import com.codecool.petshelter.model.Adoption;
+import com.codecool.petshelter.model.Pet;
+import com.codecool.petshelter.model.PetType;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -13,10 +16,12 @@ import java.util.List;
 public class AdoptionEndpoint {
 
     private AdoptionDao dao;
+    private PetDao petDao;
 
     @Inject
-    public AdoptionEndpoint(AdoptionDao dao) {
+    public AdoptionEndpoint(AdoptionDao dao, PetDao petDao) {
         this.dao = dao;
+        this.petDao = petDao;
     }
 
     @GET
@@ -59,5 +64,21 @@ public class AdoptionEndpoint {
             return Response.ok("Deleted successfully").build();
         }
         return Response.status(400).entity("Could not delete adoption").build();
+    }
+
+    @PUT
+    @Path("{adoptionId}/{petType}/{petId}")
+    public Response addPetToAdoption(@PathParam("adoptionId") long adoptionId, @PathParam("petType") String petType, @PathParam("petId") long petId) {
+        Adoption adoption = dao.getAdoptionById(adoptionId);
+        if (adoption == null) return Response.status(404).entity("There is no adoption in database with id = " + adoptionId).build();
+
+        System.out.println(petType.toUpperCase());
+        System.out.println(PetType.valueOf(petType.toUpperCase()) == PetType.DOG);
+        Pet pet = petDao.getPetById(petId, PetType.valueOf(petType.toUpperCase()));
+        if (pet == null) return Response.status(404).entity("There is no " + petType + " in database with id = " + petId).build();
+
+        adoption.setAdoptedPet(pet);
+        dao.updateAdoption(adoption);
+        return Response.ok("Pet added to adoption").build();
     }
 }
